0e32aae22ffe2573f988ed671f32ccfe9d683bbe
marloboyer <marlo.boyer7@yahoo.com>
2023-03-13 20:06:57 -0500

    Add model with seeds for inital data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Dog.create(name: "Benny", age: 2, breed: "French Bulldog")
+Dog.create(name: "Barks", age: 5, breed: "Poodle")
+Dog.create(name: "Salt", age: 3, breed: "Dashound")
+Dog.create(name: "Gliiter", age: 10, breed: "Yorkie")
+Dog.create(name: "Billy", age: 7, breed: "Lab")
